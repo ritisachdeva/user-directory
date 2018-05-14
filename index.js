8c46f5dbd0e1df9e39e0b8442100bccbcbdecbe9
Riti Sachdeva <ritiallstars@gmail.com>
2018-05-14 16:20:03 -0400

    Change paragraph text when button is clicked

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('It works!')
+const button = document.querySelector('button.greeting')
+
+function changeText() {
+    const paragraph = document.querySelector('p.greeting')
+    paragraph.textContent = 'You done clicked!'
+}
+
+button.addEventListener('click', changeText)
