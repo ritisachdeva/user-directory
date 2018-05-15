5055b9e569dd942da9b7cd7133ab62f33d5febaa
Riti Sachdeva <ritiallstars@gmail.com>
2018-05-14 22:06:37 -0400

    Part 2 done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const button = document.querySelector('button.greeting')
 const head = document.querySelector('h1')
-
+const head2 = document.querySelector('#header')
 function changeText() {
-    head.textContent = 'Clickity click'
+    head2.textContent = 'Clickity click'
 }
 
 button.addEventListener('click', changeText)
